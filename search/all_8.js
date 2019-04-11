1d65b34951798e8c86f01561733374df068ba33e
Travis CI <travis@travis-ci.com>
2019-04-11 20:03:44 +0200

    Deploy doc to GitHub Pages, Travis CI build: 14
    Commit: afb69a8cdd3e3c38e45ba5168eeb042c402a6f33

--- search/all_8.js
+++ search/all_8.js
@@ -3,5 +3,6 @@ var searchData=
   ['icommand',['ICommand',['../classICommand.html',1,'']]],
   ['invalidfilecontentexception',['InvalidFileContentException',['../structInvalidFileContentException.html',1,'']]],
   ['invalidfilenameexception',['InvalidFileNameException',['../structInvalidFileNameException.html',1,'']]],
-  ['invalidpositivenumberexception',['InvalidPositiveNumberException',['../structInvalidPositiveNumberException.html',1,'']]]
+  ['invalidpositivenumberexception',['InvalidPositiveNumberException',['../structInvalidPositiveNumberException.html',1,'']]],
+  ['invalidqueryexception',['InvalidQueryException',['../structInvalidQueryException.html',1,'']]]
 ];
